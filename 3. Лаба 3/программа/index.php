19e1dc078cf25ab32c2132992a67e5bb2b037480
53032124 <118921876+53032124@users.noreply.github.com>
2023-09-29 11:32:17 +0300

    lalaal

--- 3. Лаба 3/программа/index.php
+++ 3. Лаба 3/программа/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-3width, initial-scale=1.0">
     <?php
         // Формируем ссылку на файл стилей динамически
         $style_file = "main.css";
